2fe206a7ffd44187d81919bd2f4fbbef8185387b
Arbin Reyes <arbin.reyes@nri-anz.com>
2025-11-12 12:42:41 +0800

    fix error

--- src/gulp.plugin.js
+++ src/gulp.plugin.js
@@ -1,65 +1,49 @@
-// Export Plugin
+// Modern ES6 version of docpad-plugin-gulp (DocPad 8 compatible)
 module.exports = function (BasePlugin) {
     class GulpPlugin extends BasePlugin {
-        // Plugin name
+
         get name() {
-            return 'gulp';
+            return "gulp";
         }
 
-        // Plugin default config
-        get config() {
+        // ✅ MUST NOT define "config:" — use this instead
+        getDefaultConfig() {
             return {
                 background: false,
                 writeAfter: []
             };
         }
 
-        /**
-         * Constructor
-         */
         constructor(opts) {
             super(opts);
 
             const { docpad } = opts;
 
-            // Dependencies
-            this.safeps = require('safeps');
-            this.path = require('path');
-            this.glob = require('glob');
+            this.safeps = require("safeps");
+            this.path = require("path");
+            this.glob = require("glob");
 
-            // Attach event handlers dynamically
             this.createEventHandlers(docpad);
         }
 
-        /**
-         * Dynamically attach event handlers for any DocPad event
-         */
         createEventHandlers(docpad) {
-            const events = docpad.getEvents();
-
-            events.forEach(eventName => {
+            docpad.getEvents().forEach(eventName => {
                 this[eventName] = (opts, next) => {
-                    const tasks = this.getConfig()[eventName] || false;
+                    const tasks = this.getConfig()[eventName];
 
                     if (tasks) {
                         this.processGulp(tasks, opts, next);
                     } else {
-                        return next();
+                        next();
                     }
                 };
             });
-
-            return this;
         }
 
-        /**
-         * Execute gulp tasks
-         */
         processGulp(tasks, opts, next) {
             const rootPath = this.docpad.getConfig().rootPath;
 
-            // Locate gulp CLI inside node_modules
-            const files = this.glob.sync('**/gulp/bin/gulp.js', {
+            const files = this.glob.sync("**/gulp/bin/gulp.js", {
                 cwd: rootPath,
                 nosort: true
             });
@@ -67,20 +51,17 @@ module.exports = function (BasePlugin) {
             const gulpPath = files[0];
 
             if (!gulpPath) {
-                return next(new Error('Could not find the gulp command line interface.'));
+                return next(new Error("Could not find the gulp command line interface."));
             }
 
-            // Build command
             const command = [
                 this.path.join(rootPath, gulpPath),
-                ...(tasks || [])
+                ...tasks
             ];
 
             if (!this.getConfig().background) {
-                // Synchronous with output
                 this.safeps.spawn(command, { cwd: rootPath, output: true }, next);
             } else {
-                // Fire-and-forget background task
                 this.safeps.spawn(command, { cwd: rootPath, output: true });
                 next();
             }
